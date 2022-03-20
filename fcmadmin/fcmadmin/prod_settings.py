997987db38b2505c6be35f62710df3bf275454ac
MyriamBel <nikitokmalishok@mail.ru>
2022-03-20 18:37:49 +0300

    Save changes

--- fcmadmin/fcmadmin/prod_settings.py
+++ fcmadmin/fcmadmin/prod_settings.py
@@ -1,18 +1,19 @@
 import os
 from pathlib import Path
 
-import cloudinary
-import dj_database_url
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fcmadminDatabase',
+        'USER': 'fcmadminDBAdmin',
+        'PASSWORD': 'postgres1',
+        'HOST': 'localhost',
+        'PORT': '5432',
         'CONN_MAX_AGE': 60 * 10,  # 10 minutes
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=360)
-DATABASES['default'].update(db_from_env)
 
 with open(os.path.join(BASE_DIR, 'fcm-admin/secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
