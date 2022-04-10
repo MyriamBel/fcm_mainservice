c09dd722bc1c2f0063addcfaad6c46af3a6194de
MyriamBel <nikitokmalishok@mail.ru>
2022-04-10 20:44:07 +0300

    Аутентификация сотрудника заведения в приложении кассира/администратора заведения.

--- fcmadmin/Shifts/urls.py
+++ fcmadmin/Shifts/urls.py
@@ -3,10 +3,10 @@ from .views import cashier_authentication
 
 app_name = "Shifts"
 
-shifts_patterns = [
-    path('', cashier_authentication),
+auth_patterns = [
+    path('access/', cashier_authentication),
 ]
 
 urlpatterns = [
-    path('', include(shifts_patterns)),
+    path('tokens/', include(auth_patterns)),
 ]
